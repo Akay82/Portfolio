fbe5be01ac3eaa43a854ecdf2f32c9f76ed85f37
Ajay Kumar <95285802+Akay82@users.noreply.github.com>
2025-11-01 11:48:55 +0530

    Update resume PDF file path

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Ajay_Resume.pdf"; // Adjust the path as per your file structure
+import pdf from "../../Assets/Ajay_Kumar_Resume.pdf"; // Adjust the path as per your file structure
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
