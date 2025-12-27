b1eecf16deb5bce7a259a298e586067a18567a01
Akay82 <youremail@yourdomain.com>
2025-12-27 20:31:59 +0530

    fix

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,8 +4,6 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import weather from "../../Assets/Projects/weather.png";
 import drawing from "../../Assets/Projects/drawing.png";
-import text from "../../Assets/Projects/Text.png"
-import login from "../../Assets/Projects/login.png"
 import cloude5 from "../../Assets/Projects/cloude5.png"
 import shop from "../../Assets/Projects/shop.png";
 import todo from "../../Assets/Projects/todo.png";
